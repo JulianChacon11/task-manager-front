b17932b82b5acfa89c1bdfadf126f5fd4eb3dca4
Armando Alamo <armandoalamo@seti.com.co>
2025-05-20 12:31:38 -0500

    Update projects-page.component.ts

--- src/app/features/projects/pages/projects-page/projects-page.component.ts
+++ src/app/features/projects/pages/projects-page/projects-page.component.ts
@@ -5,21 +5,21 @@ import { Project } from '../../interfaces/project.interface';
 
 const projects: Project[] = [
   {
-    id: 1,
+    id: "1",
     nombre: 'Project 1',
     descripcion: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Inventore, laudantium incidunt vitae facere natus sunt ratione neque voluptatem fugiat cumqu',
     fechaInicio: new Date('2025-05-20'),
     fechaFin: new Date('2025-05-22')
   },
   {
-    id: 2,
+    id: "2",
     nombre: 'Project 2',
     descripcion: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Inventore, laudantium incidunt vitae facere natus sunt ratione neque voluptatem fugiat cumqu',
     fechaInicio: new Date('2025-05-20'),
     fechaFin: new Date('2025-05-22')
   },
   {
-    id: 3,
+    id:"3" ,
     nombre: 'Project 3',
     descripcion: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Inventore, laudantium incidunt vitae facere natus sunt ratione neque voluptatem fugiat cumqu',
     fechaInicio: new Date('2025-05-20'),
